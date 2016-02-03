e28cefde97f71c419a21641280eef2bb1acd1bdf
Andy Tran <hello@andytran.me>
2016-02-02 16:54:43 -0800

    Added in custom scripts option.

--- functions.php
+++ functions.php
@@ -2,6 +2,6 @@
 	add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 	function theme_enqueue_styles() {
 	    wp_enqueue_style( 'divi', get_template_directory_uri() . '/style.css' );
-
+      wp_enqueue_script( 'divi', plugin_dir_url( __FILE__ ) . 'js/scripts.js', array( 'jquery', 'divi-custom-script' ), '0.1.2', true );
 	}
 ?>
